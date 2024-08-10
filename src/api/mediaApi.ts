5744eb1cfb9e325d7832a97b7ca7f66210b79f7b
sashkashhh <chenneldps@gmail.com>
2024-08-10 05:31:36 +0300

    хост10

--- src/api/mediaApi.ts
+++ src/api/mediaApi.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getMediaByEvent = async (eventId: string) => {
   try {
-    const response = await axios.get('http://5.35.44.198:5000/api/media/getByEvent', { params: {eventId}})
+    const response = await axios.get('http://v101242.hosted-by-vdsina.com:5000/api/media/getByEvent', { params: {eventId}})
     return response.data
   } catch (error) {
     console.log(error)
